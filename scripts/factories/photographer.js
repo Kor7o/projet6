7a7aa255e6b7855cc4568c0bc8e10cbd61d93baf
Kor7o <dodane.hugo@gmail.com>
2023-05-04 15:04:19 +0200

    All datas dl, classes created,

--- scripts/factories/photographer.js
+++ scripts/factories/photographer.js
@@ -1,28 +1,42 @@
 function photographerFactory(data) {
-    const { name, city, country, tagline, price, portrait } = data;
+      
+    // Destructuring the data object to extract its properties
+    const { name, city, country, tagline, price, portrait, id } = data;
 
+    // Creating the path for the portrait image
     const picture = `assets/photographers/${portrait}`;
 
+    // Defining a function that will return a DOM element for the photographer card
     function getUserCardDOM() {
         
+        // Create an article element to contain the photographer card
         const article = document.createElement( 'article' );
-       
+        article.className += "photographer-card";
+    
+        
+        // Create an image element for the photographer portrait
         const img = document.createElement( 'img' );
-        img.setAttribute("src", picture)
-       
+        img.setAttribute("src", picture) //j'ai essaye de mettre `picture ${name}` mais ca ne marche pas
+        img.className += "photographer-img";
+
         const h2 = document.createElement( 'h2' );
         h2.textContent = name;
+        h2.className += "photographer-name";
 
         const location = document.createElement( 'p' );
         location.textContent = `${city}, ${country}`;
+        location.className += "photographer-location";
+
 
         const tag = document.createElement( 'p' );
         tag.textContent = tagline;
-  
+        tag.className += "photographer-tagline";
+
         const rate = document.createElement("p");
         rate.textContent = `${price} € / jour`;
-       
+        rate.className += "photographer-rate";
 
+       
         article.appendChild(img);
         article.appendChild(h2);
         article.appendChild(location);
--- scripts/pages/index.js
+++ scripts/pages/index.js
@@ -1,30 +1,8 @@
-    async function getPhotographers() {
-        // Ceci est un exemple de données pour avoir un affichage de photographes de test dès le démarrage du projet, 
-        // mais il sera à remplacer avec une requête sur le fichier JSON en utilisant "fetch".
-        let photographers = [
-            {
-                "name": "Mimi Keel",
-			    "id": 243,
-			    "city": "London",
-			    "country": "UK",
-			    "tagline": "Voir le beau dans le quotidien",
-			    "price": 400,
-			    "portrait": "MimiKeel.jpg"
-            },
-            {
-                "name": "Ellie-Rose Wilkens",
-			    "id": 930,
-			    "city": "Paris",
-			    "country": "France",
-			    "tagline": "Capturer des compositions complexes",
-			    "price": 250,
-			    "portrait": "EllieRoseWilkens.jpg"
-            },
-        ]
-        // et bien retourner le tableau photographers seulement une fois récupéré
-        return ({
-            photographers: [...photographers, ...photographers, ...photographers]})
-    }
+async function getPhotographers() {
+    // Récupère les données des photographes depuis le fichier JSON
+    return fetch('data/photographers.json')
+        .then(response => response.json());
+}
 
     async function displayData(photographers) {
         const photographersSection = document.querySelector(".photographer_section");
